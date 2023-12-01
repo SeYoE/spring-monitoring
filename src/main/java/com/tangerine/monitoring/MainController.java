64f77c6d2f6cddf3b9bed017900fd877203a700d
SeYoE <onetuks@naver.com>
2023-12-02 02:03:16 +0900

    Feat: 테스트 용 API 추가

--- src/main/java/com/tangerine/monitoring/MainController.java
+++ src/main/java/com/tangerine/monitoring/MainController.java
@@ -1,2 +1,21 @@
-package com.tangerine.monitoring;public class MainController {
+package com.tangerine.monitoring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/monitoring")
+public class MainController {
+
+    @GetMapping(path = "/")
+    public String main() {
+        return "main";
+    }
+
+    @GetMapping(path = "/endpoint")
+    public String endpoint() {
+        return "endpoint";
+    }
+
 }
